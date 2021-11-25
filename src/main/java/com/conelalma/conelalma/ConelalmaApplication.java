ca28b258ae7f50bb6d2cebdada465f3d498768a8
hanzemendoza <hanzemendoza@est.frba.utn.edu.ar>
2021-11-25 19:19:48 -0300

    creación del proyecto

---
+++ src/main/java/com/conelalma/conelalma/ConelalmaApplication.java
@@ -0,0 +1,13 @@
+package com.conelalma.conelalma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConelalmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConelalmaApplication.class, args);
+	}
+
+}
