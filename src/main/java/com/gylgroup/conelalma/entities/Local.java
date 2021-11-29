9a2e9bd6812098ce0e4e9c2464d82427b79efa08
jesus <tejerina.jesus.ed@gmail.com>
2021-11-29 20:05:51 -0300

    comment

---
+++ src/main/java/com/gylgroup/conelalma/entities/Local.java
@@ -0,0 +1,26 @@
+package com.gylgroup.conelalma.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Local {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String direccion;
+    private Integer cantidadMaximaPersonas;
+    private Double precio;
+    private String foto;
+}
---
+++ src/main/java/com/gylgroup/conelalma/entities/Usuario.java
@@ -0,0 +1,33 @@
+package com.gylgroup.conelalma.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nombre;
+    private String apellido;
+    private String celular;
+
+    @ManyToOne
+    private Rol rol;
+
+    private String email;
+    private String contrasenia;
+    private String foto;
+}
