49e78053667a14f6233d00007e1e77e1fdc52d50
Maximiliano Messore <maximilianomessore@gmail.com>
2021-11-30 19:44:35 -0300

    Se agrego Java Validation en el model Local

--- src/main/java/com/gylgroup/conelalma/entities/Local.java
+++ src/main/java/com/gylgroup/conelalma/entities/Local.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,9 +21,12 @@ public class Local {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     
-    
+    @NotNull(message ="La direccion es obligatoria")
+    @NotEmpty(message ="La direccion es obligatoria")
     private String direccion;
+    @NotNull(message ="La cantidad de Personas no puede ser nulla")
     private Integer cantidadMaximaPersonas;
+    @NotNull(message = "El precio no puede ser nullo")
     private Double precio;
     private String foto;
 }
