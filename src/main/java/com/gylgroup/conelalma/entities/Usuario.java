b415eda6d7a642a0cca490964eadf3d3e541354c
jesus <tejerina.jesus.ed@gmail.com>
2021-11-30 20:39:18 -0300

    service user, falta validation!

--- src/main/java/com/gylgroup/conelalma/entities/Usuario.java
+++ src/main/java/com/gylgroup/conelalma/entities/Usuario.java
@@ -1,16 +1,25 @@
 package com.gylgroup.conelalma.entities;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor
 @Getter
 @Setter
@@ -30,4 +39,13 @@ public class Usuario {
     private String email;
     private String contrasenia;
     private String foto;
+    private Boolean estado;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime fechaCreacion;
+
+    @LastModifiedDate
+    private LocalDateTime fechaModificacion;
+
 }
