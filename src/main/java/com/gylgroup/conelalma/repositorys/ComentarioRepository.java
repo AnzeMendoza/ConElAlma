3b5551e6f92a3f5c127b6c5b18535dbbf2833926
DESKTOP-ISTA7PU\reboo <meza.maximiliano364@gmail.com>
2021-11-30 09:31:28 -0300

    Repo y service ok

---
+++ src/main/java/com/gylgroup/conelalma/repositorys/ComentarioRepository.java
@@ -0,0 +1,31 @@
+package com.gylgroup.conelalma.repositorys;
+
+import com.gylgroup.conelalma.entities.Comentario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ComentarioRepository extends JpaRepository<Comentario,Integer> {
+
+    @Modifying
+    @Query(value = "UPDATE comentario SET estado = true WHERE id = ?1", nativeQuery = true)
+    void habilitar(Integer id);
+
+    @Query(value = "SELECT * FROM comentario WHERE estado=true", nativeQuery = true)
+    List<Comentario> traerAltas();
+
+    @Query(value = "SELECT * FROM comentario WHERE usuario_id = ?1 ",nativeQuery = true)
+    List<Comentario> traerPorUsuario(Integer usuario_id);
+
+    @Query(value = "SELECT * FROM comentario WHERE reserva_id = ?1 ",nativeQuery = true)
+    List<Comentario> traerPorReserva(Integer reserva_id);
+
+    Optional<Comentario> findById(Integer id);
+
+    Boolean existsComentarioById(Integer id);
+}
---
+++ src/test/java/com/conelalma/conelalma/ConelalmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conelalma.conelalma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConelalmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
