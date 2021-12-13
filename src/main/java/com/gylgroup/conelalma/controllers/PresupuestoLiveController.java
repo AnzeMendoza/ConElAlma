f7a4eb78bab77ba2fc48c127cfb94fcd1a138c47
hanzemendoza <hanzemendoza@est.frba.utn.edu.ar>
2021-12-13 11:20:47 -0300

    la validación funciona correctamente

--- src/main/java/com/gylgroup/conelalma/controllers/PresupuestoLiveController.java
+++ src/main/java/com/gylgroup/conelalma/controllers/PresupuestoLiveController.java
@@ -5,10 +5,10 @@ import com.gylgroup.conelalma.enums.TipoEvento;
 import com.gylgroup.conelalma.services.PresupuestoLiveService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.validation.Valid;
@@ -24,51 +24,45 @@ public class PresupuestoLiveController {
 
     @GetMapping("/")
     public ModelAndView listar() {
-        ModelAndView mav = new ModelAndView("prestamoListar");
+        ModelAndView mav = new ModelAndView("presupuestoListar");
         mav.addObject("titulo", "Listado de todos los presupuestos");
         mav.addObject("presupuestos", presupuestoLiveService.findAll());
         mav.addObject("presupuestosActivos", presupuestoLiveService.findByEstadoTrue());
+        mav.addObject("presupuestoConErrores", null);
         return mav;
     }
 
-    @GetMapping("/formulario/{id}")
-    public ModelAndView mostrarFormulario(@PathVariable("id") int id) throws Exception {
-        ModelAndView mav = new ModelAndView("prestamoFormulario");
-        mav.addObject("titulo", "Formulario de ingreso de prestamo");
-        mav.addObject("tiposDeEventos", new ArrayList<>(EnumSet.allOf(TipoEvento.class)));
+    @GetMapping("/agregar/{id}")
+    public String prestamoFormulario(Model model, @PathVariable("id") int id) throws Exception {
+        model.addAttribute("titulo", "Formulario de presupuesto");
+        model.addAttribute("tiposDeEventos", new ArrayList<>(EnumSet.allOf(TipoEvento.class)));
 
-        if (id == 0 ) {
-            mav.addObject("presupuesto", new PresupuestoLive());
+        if (id == 0) {
+            model.addAttribute("presupuesto", new PresupuestoLive());
         } else {
-            mav.addObject("presupuesto", presupuestoLiveService.findById(id));
+            model.addAttribute("presupuesto", presupuestoLiveService.findById(id));
         }
-        return mav;
+        return "presupuestoFormulario";
     }
 
-    @PostMapping("/formulario/{id}")
-    public RedirectView cargarFormulario(@PathVariable("id") int id,
-                                   @Valid @ModelAttribute("prespuesto") PresupuestoLive presupuestoLive,
-                                   BindingResult result,
-                                   RedirectAttributes redirectAttributes) throws Exception {
+    @PostMapping("/agregar/{id}")
+    public String prestamoAlta(Model model,
+                            @Valid @ModelAttribute("presupuesto") PresupuestoLive presupuesto,
+                            BindingResult result,
+                            @PathVariable("id") int id) throws Exception {
         if (result.hasErrors()) {
-            redirectAttributes.addFlashAttribute(presupuestoLive);
-            return new RedirectView("/presupuesto/formulario/"+0);
+            model.addAttribute("titulo", "Formulario de presupuesto");
+            model.addAttribute("tiposDeEventos", new ArrayList<>(EnumSet.allOf(TipoEvento.class)));
+            return "presupuestoFormulario";
         }
 
         if (id == 0) {
-            presupuestoLiveService.save(presupuestoLive);
+            presupuestoLiveService.save(presupuesto);
         } else {
-            presupuestoLiveService.update(presupuestoLive, id);
+            presupuestoLiveService.update(presupuesto, id);
         }
-        return new RedirectView("/presupuesto/");
+        return "redirect:/presupuesto/";
     }
-
-    /*@GetMapping("/{id}")
-    public ModelAndView listarUno(@PathVariable("id") int id) throws Exception {
-        ModelAndView mav = new ModelAndView("prestamoListar");
-        mav.addObject("prestamos", presupuestoLiveService.findById(id));
-        return mav;
-    }*/
 
     @GetMapping("/activar/{id}")
     public RedirectView activar(@PathVariable("id") int id) {
